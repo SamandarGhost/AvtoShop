036dedd78640947250176043ccaa07da19ac0ffb
Sam <kimjeyeon03@icloud.com>
2025-02-05 21:34:48 +0900

    fix: modify main.ts

--- apps/avto-shop/src/main.ts
+++ apps/avto-shop/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   app.enableCors({ origin: true, credentials: true });
 
   app.use(graphqlUploadExpress({ maxFileSize: 15000000, maxFiles: 10 }));
-  app.use('uploads', express.static('./uploads'))
+  app.use('/uploads', express.static('./uploads'));
 
   app.useWebSocketAdapter(new WsAdapter(app));
   await app.listen(process.env.AVTO_SHOP_PORT_API ?? 3004);
